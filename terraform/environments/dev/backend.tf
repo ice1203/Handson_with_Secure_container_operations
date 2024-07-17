0ba19e10b82806eec7fcb0e97451e3519f3fe1a7
ice1203 <kikuchi3588@gmail.com>
2024-07-17 19:56:23 +0900

    backend変更他

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    key                        = "module2/ecs/tfstate"
+    key                        = "handson/ecs/tfstate"
     region                     = "ap-northeast-1"
     terraform_state_lock_table = "tmp-hands-on-tf-state-lock"
   }
