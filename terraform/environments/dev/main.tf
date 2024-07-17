bd498e23ca9989f6220e952d9f6cc0c568927d75
ice1203 <kikuchi3588@gmail.com>
2024-07-17 11:21:59 +0900

    修正

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,7 +1,6 @@
 data "aws_caller_identity" "current" {}
 locals {
-  #sys_name       = "ecs"
-  sys_name       = "ecs4"
+  sys_name       = "ecs"
   env_name       = "handson"
   aws_account_id = data.aws_caller_identity.current.account_id
 }
