c8f76c7808c9a2c5b4b36bdafe220eab4d1d2631
ice1203 <kikuchi3588@gmail.com>
2024-07-18 11:56:44 +0900

    sysdigオーケストレーター追加

--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -4,3 +4,16 @@ variable "allowed_cidr_blocks" {
   default     = ["0.0.0.0/0"]
 
 }
+
+variable "sysdig_agent_access_key" {
+  type        = string
+  description = "Sysdig agent access key"
+  sensitive   = true
+
+}
+#variable "secure_api_token" {
+#  type        = string
+#  description = "Sysdig Secure API token"
+#  sensitive = true
+#
+#}
